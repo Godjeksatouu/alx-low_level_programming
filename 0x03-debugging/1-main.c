df264d538c43743c511e554b57cfaca650a2480c
Godjeksatouu <godjeksatou@gmail.com>
2023-08-17 09:30:26 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,9 +1,7 @@
-#include <stdio.h>
-
 /**
- * main - causes an infinite loop
- * Return: 0
- */
+* main - causes an infinite loop
+* Return: 0
+*/
 
 int main(void)
 {
@@ -15,10 +13,10 @@ int main(void)
 
 	/*while (i < 10)*/
 	/*{*/
-	/*    putchar(i);*/
+	/*	 putchar(i);*/
 	/*}*/
 
 	printf("Infinite loop avoided! \\o/\n");
 
-	 return (0);
+	return (0);
 }
