bcf86b3029ecbd773a9b92480d5c463baac9cc84
Godjeksatouu <godjeksatou@gmail.com>
2023-09-12 14:55:50 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's basic info
+ * @nale: dog's name
+ * @age: dog's name
+ * @owner: owner's name
+ *
+ * Description: just a lone dog struct in a big kitty world
+ */
+struct dog
+{
+		char *name;
+		float age;
+		char *owner;
+};
+
+#endif
