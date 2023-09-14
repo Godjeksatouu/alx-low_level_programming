b065d9d41b1b2dadb5c8eaace7fba3137ac47829
Godjeksatouu <godjeksatou@gmail.com>
2023-09-14 02:34:37 +0200

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,16 +7,15 @@
  *     * @action: pointer to print in regular or hex
  *      * Return: void
  *       */
-
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 		unsigned int i;
 
-		if (array == NULL || action == NULL)
-			return;
+			if (array == NULL || action == NULL)
+						return;
 
-		for (i = 0; i < size; i++)
-		{
-		action(array[i]);										}
-		}
+				for (i = 0; i < size; i++)
+						{
+									action(array[i]);
+										}
 }
